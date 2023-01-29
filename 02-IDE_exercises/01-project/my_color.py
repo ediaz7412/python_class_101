f51b12e3c968aa386a750360c9d237f3b3f95edf
ediaz7412 <erick.diaz7@icloud.com>
2023-01-28 21:24:49 -0800

    first project completed

---
+++ 02-IDE_exercises/01-project/my_color.py
@@ -0,0 +1,7 @@
+# Author: Erick Diaz
+# Date: 01/28/2023
+# Description: Asks the user for their favorite color and then
+#              prints out a statement with the variable
+
+fave_color = input("Please enter your favorite color: ")
+print("Your favorite color is  " + fave_color + ".")
